3428bea237aaf349d03c8a8b40d141166c61f474
Pourya Daneshvar <pourya@daneshvar.me>
2026-04-13 21:54:37 +0330

    Added new features

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ let sections = document.getElementsByClassName("section");
 for (let section of sections) {
     const title = section.getElementsByClassName("title");
     title.item(0)?.addEventListener("click", () => {
-        hideAllSection(sections);
-        section.classList.add("active");
+        // hideAllSection(sections);
+        if (section.classList.contains('active')) {
+            section.classList.remove("active");
+        } else {
+            hideAllSection(sections)
+            section.classList.add("active");
+        }
+        window.scrollTo(0, 0)
     });
 }
 
